54e49c804f320afd481a4c492cce2407559e4e9e
Serge Aleshin-Guendel <aleshing@uw.edu>
2023-01-18 15:45:56 -0800

    Update readme

--- R/multilink-package.R
+++ R/multilink-package.R
@@ -1,9 +1,9 @@
 #' Multifile Record Linkage and Duplicate Detection
 #'
 #' The multilink package implements the methodology of Aleshin-Guendel & Sadinle
-#' (2022) <doi:10.1080/01621459.2021.2013242>. It handles the general problem of
-#' multifile record linkage and duplicate detection, where any number of files
-#' are to be linked, and any of the files may have duplicates.
+#' (2022). It handles the general problem of multifile record linkage and
+#' duplicate detection, where any number of files are to be linked, and any of
+#' the files may have duplicates.
 #'
 #' @references Serge Aleshin-Guendel & Mauricio Sadinle (2022). Multifile Partitioning for Record Linkage and Duplicate Detection. \emph{Journal of the
 #' American Statistical Association}. [\href{https://doi.org/10.1080/01621459.2021.2013242}{Published}] [\href{https://arxiv.org/abs/2110.03839}{arXiv}]
